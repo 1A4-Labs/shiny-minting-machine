eb24e7c3b9f3ca443da328b29902b257e2ef08d1
jazkamer <v.rodionov@protonmail.com>
2023-03-28 23:21:39 +0300

    yo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['MetaplexProvider.js', 'MintNFTs.js', '_app.js', 'index.js', 'useMetaplex.js'],
+  pageExtensions: ['MetaplexProvider.js', 'MintNFTs.js', 'useMetaplex.js'],
 }
 
 module.exports = nextConfig
